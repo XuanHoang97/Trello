8a907efeb17709a7ca6850ef262cb5cf90aabf9c
ADMIN\Admin <xuanhoang997@gmail.com>
2022-04-12 20:29:39 +0700

    init backEnd trello

---
+++ nodejs/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
